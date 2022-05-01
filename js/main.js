2a5c12b5822089a94c0196b68100f6e6e32afd01
calgil <clgilbert1105@gmail.com>
2022-05-01 16:48:00 -0600

    ugh

--- js/main.js
+++ js/main.js
@@ -1,79 +1,90 @@
 
+const isVisible = 'is-visible';
+const modalOpen = 'data-open';
 
 const container = document.querySelector('.parks');
 
 const stateFilter = document.querySelector('.state-filter');
 
-stateFilter.addEventListener('change', (e) => {
-    const value = e.target.value.toLowerCase();
-    const parksByState = fetch(`https://developer.nps.gov/api/v1/parks?stateCode=${value}&api_key=2hL7WMh7PeKnrwR39LONcMrAMvibH0MiBL8QMMSH`);
-    parksByState
-.then((res) => res.json())
-.then((res) => { 
-    container.innerHTML = '';
-    res.data.forEach(park => {
-        const {fullName, directionsUrl, description} = park;
-        const {altText, url} = park.images[0];
-        const {modalAltText, modalUrl} = park.images[1];
-        const parkCard = document.createElement('div');
-        parkCard.classList.add('park');
-        parkCard.innerHTML = `
-        <div class="img-wrapper">
-            <img class="park-img" src="${url}" alt ="${altText}">
-        </div>
-       <div class="card-text">
-            <p class ="park-name">
-            ${fullName}
-            </p>
-       </div>
-       `
-    //    <div class="modal">
-    //     <div class="modal-img">
-    //         <img src="${modalUrl}" alt="${modalAltText}">
-    //     </div>
-    //     <div class="park-details">
-    //         <p class="description">
-    //             ${description}
-    //         </p>
-    //         <p class="directions">
-    //             <a class="directions" href="${directionsUrl}"> Directions </a> 
-    //         </p>
-    //     </div>
-    //    </div>
-        container.appendChild(parkCard)
-    });
-});
-})
+// Part that works
 
- 
-
-
-
-
-
-
-// Where I am trying to get to the data I want
-// parksByState
+// stateFilter.addEventListener('change', (e) => {
+//     const value = e.target.value.toLowerCase();
+//     const parksByState = fetch(`https://developer.nps.gov/api/v1/parks?stateCode=${value}&api_key=2hL7WMh7PeKnrwR39LONcMrAMvibH0MiBL8QMMSH`);
+//     parksByState
 // .then((res) => res.json())
 // .then((res) => { 
+//     container.innerHTML = '';
 //     res.data.forEach(park => {
-//         const {fullName, directionsUrl} = park;
-//         const {altText, url} = park.images[0];
-//         console.log(fullName, directionsUrl);
-//     });
-// });
-
-
-// Where I am trying to display data as html
-
-// parksByState
-// .then((res) => res.json())
-// .then((res) => { 
-//     res.data.forEach(park => {
-//         const {fullName, directionsUrl} = park;
+//         const {fullName, parkCode} = park;
 //         const {altText, url} = park.images[0];
 //         const parkCard = document.createElement('div');
-//         parkCard.classList.add('park');
+//         parkCard.classList.add(`park`);
+//         parkCard.innerHTML = `
+//         <div class="img-wrapper">
+//             <img class="park-img" src="${url}" alt ="${altText}">
+//         </div>
+//        <div class="card-text" data-open="${[parkCode]}">
+//             <p class ="park-name">
+//             ${fullName}
+//             </p>
+//        </div>
+//        `
+//         container.appendChild(parkCard);
+//     });
+// });
+// })
+
+// Part I'm messing with
+
+const openModal = () => {
+    const parks = document.querySelectorAll(modalOpen);
+    console.log(parks);
+    // parks.addEventListener('click' , )
+}
+
+stateFilter.addEventListener('change', (e) => {
+    const value = e.target.value.toLowerCase();
+    const endpoint = fetch(`https://developer.nps.gov/api/v1/parks?stateCode=${ value }&api_key=2hL7WMh7PeKnrwR39LONcMrAMvibH0MiBL8QMMSH`);
+    endpoint
+        .then((res) => res.json())
+        .catch((err) => console.log(err))
+        .then((res) => { 
+            container.innerHTML = '';
+            res.data.forEach(park => {
+                const {fullName, parkCode} = park;
+                const {altText, url} = park.images[0];
+                const parkCard = document.createElement('div');
+                parkCard.classList.add(`park`);
+                parkCard.innerHTML = `
+                <div class="img-wrapper">
+                    <img class="park-img" src="${url}" alt ="${altText}">
+                </div>
+            <div class="card-text" data-open="${[parkCode]}">
+                    <p class ="park-name">
+                    ${fullName}
+                    </p>
+            </div>
+            `
+                container.appendChild(parkCard);
+            });
+        });
+        openModal()
+    })
+
+
+// stateFilter.addEventListener('change', (e) => {
+    // const value = e.target.value.toLowerCase();
+    // const parksByState = fetch(`https://developer.nps.gov/api/v1/parks?stateCode=${value}&api_key=2hL7WMh7PeKnrwR39LONcMrAMvibH0MiBL8QMMSH`);
+// parksByState
+// .then((res) => res.json())
+// .then((res) => { 
+//     container.innerHTML = '';
+//     res.data.forEach(park => {
+//         const {fullName, parkCode} = park;
+//         const {altText, url} = park.images[0];
+//         const parkCard = document.createElement('div');
+//         parkCard.classList.add('park', `data-open="${[parkCode]}"`);
 //         parkCard.innerHTML = `
 //         <div class="img-wrapper">
 //             <img class="park-img" src="${url}" alt ="${altText}">
@@ -82,10 +93,31 @@ stateFilter.addEventListener('change', (e) => {
 //             <p class ="park-name">
 //             ${fullName}
 //             </p>
-//             <a class="directions" href="${directionsUrl}"> Directions </a>
 //        </div>
-       
-//         `
-//         container.appendChild(parkCard)
+//        `
+//         container.appendChild(parkCard);
 //     });
 // });
+
+// })
+
+// setTimeout(() => {
+//     const openModal = document.querySelectorAll('.park');
+
+//     console.log(openModal);
+// }, 2000)
+
+
+
+
+// for (const elm of openModal) {
+//     console.log(elm);
+//     // elm.addEventListener('click', function(){
+//     //     const parkCode = this.dataset.open;
+//     //     this.classList.add(isVisible)
+//     // })
+// }
+
+
+
+
