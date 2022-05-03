ce82964e639cdf008730c782721ea1cfb42ad79b
calgil <clgilbert1105@gmail.com>
2022-05-03 15:13:05 -0600

    click events added

--- js/main.js
+++ js/main.js
@@ -1,48 +1,42 @@
 
 const isVisible = 'is-visible';
-const modalOpen = 'data-open';
+const modalOpen = '[data-open]';
+const modalClose = '[data-close]';
+const modalBg = '.park-modal';
 
 const container = document.querySelector('.parks');
+const modalContainer = document.querySelector('.modal-box');
 
 const stateFilter = document.querySelector('.state-filter');
 
-// Part that works
 
-// stateFilter.addEventListener('change', (e) => {
-//     const value = e.target.value.toLowerCase();
-//     const parksByState = fetch(`https://developer.nps.gov/api/v1/parks?stateCode=${value}&api_key=2hL7WMh7PeKnrwR39LONcMrAMvibH0MiBL8QMMSH`);
-//     parksByState
-// .then((res) => res.json())
-// .then((res) => { 
-//     container.innerHTML = '';
-//     res.data.forEach(park => {
-//         const {fullName, parkCode} = park;
-//         const {altText, url} = park.images[0];
-//         const parkCard = document.createElement('div');
-//         parkCard.classList.add(`park`);
-//         parkCard.innerHTML = `
-//         <div class="img-wrapper">
-//             <img class="park-img" src="${url}" alt ="${altText}">
-//         </div>
-//        <div class="card-text" data-open="${[parkCode]}">
-//             <p class ="park-name">
-//             ${fullName}
-//             </p>
-//        </div>
-//        `
-//         container.appendChild(parkCard);
-//     });
-// });
-// })
 
-// Part I'm messing with
+const closeModal = () => {
+    document.addEventListener('keyup', (e) => {
+        if(e.key === 'Escape'){
+            modalContainer.innerHTML = '';
+        }
+    });
+    document.addEventListener('click', (e) => {
+        if(e.target === document.querySelector(modalBg)){
+            modalContainer.innerHTML = '';
+        }
+    })
+    const closeModalButton = document.querySelector(modalClose);
+    closeModalButton.addEventListener('click', () => {
+        modalContainer.innerHTML = '';
+    });
+};
 
-const openModal = () => {
-    const parks = document.querySelectorAll(modalOpen);
-    console.log(parks);
-    // parks.addEventListener('click' , )
+const getParkCode = () => {
+    let openModal = container.childNodes;
+    openModal.forEach((park) => {
+        park.addEventListener('click', (e) => {
+            parkCode = e.target.dataset.open;
+            makeModal(parkCode);
+        })
+    })
 }
-
 stateFilter.addEventListener('change', (e) => {
     const value = e.target.value.toLowerCase();
     const endpoint = fetch(`https://developer.nps.gov/api/v1/parks?stateCode=${ value }&api_key=2hL7WMh7PeKnrwR39LONcMrAMvibH0MiBL8QMMSH`);
@@ -60,63 +54,63 @@ stateFilter.addEventListener('change', (e) => {
                 <div class="img-wrapper">
                     <img class="park-img" src="${url}" alt ="${altText}">
                 </div>
-            <div class="card-text" data-open="${[parkCode]}">
+                <div class="card-text">
                     <p class ="park-name">
                     ${fullName}
                     </p>
-            </div>
+                </div>
+                <div class="card-popup" data-open="${parkCode}">
+                    <p>Click to learn more about ${fullName}</p>
+                </div>
             `
                 container.appendChild(parkCard);
-            });
-        });
-        openModal()
+            })
+        })
+        .then( ()=> getParkCode());
     })
 
+const makeModal = (parkCode) => {
+    const endpoint = fetch(`https://developer.nps.gov/api/v1/parks?parkCode=${ parkCode }&api_key=2hL7WMh7PeKnrwR39LONcMrAMvibH0MiBL8QMMSH`);
+    endpoint
+    .then((res) => res.json())
+    .catch((err) => console.log(err))
+    .then((res) => {
+        res.data.forEach(park => {
+            const {fullName, description, directionsUrl} = park;
+            const {altText, url} = park.images[1];
+            const parkModal = document.createElement('div');
+            parkModal.classList.add('park-modal');
+            parkModal.innerHTML = `
+            
+            <div class="modal-dialog">
+                <div class="modal-header">
+                     <h3> ${fullName} </h3>
+                     <i class="fas fa-times" data-close></i>
+                </div>
+                <div class="modal-body">
+                    <div class="modal-img-wrapper">
+                        <img src="${ url }" alt="${ altText }">
+                    </div>
+                    <div class="text-wrapper">
+                        <p>
+                            ${description}
+                        </p>
+                        <div class="modal-links">
+                            <a class="directions" href="${directionsUrl}">Directions</a>
+                            <i class="fa-solid fa-heart"></i>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+            `
+            modalContainer.appendChild(parkModal);
+        })
+    })
+    .then(() => closeModal());
+}
 
-// stateFilter.addEventListener('change', (e) => {
-    // const value = e.target.value.toLowerCase();
-    // const parksByState = fetch(`https://developer.nps.gov/api/v1/parks?stateCode=${value}&api_key=2hL7WMh7PeKnrwR39LONcMrAMvibH0MiBL8QMMSH`);
-// parksByState
-// .then((res) => res.json())
-// .then((res) => { 
-//     container.innerHTML = '';
-//     res.data.forEach(park => {
-//         const {fullName, parkCode} = park;
-//         const {altText, url} = park.images[0];
-//         const parkCard = document.createElement('div');
-//         parkCard.classList.add('park', `data-open="${[parkCode]}"`);
-//         parkCard.innerHTML = `
-//         <div class="img-wrapper">
-//             <img class="park-img" src="${url}" alt ="${altText}">
-//         </div>
-//        <div class="card-text">
-//             <p class ="park-name">
-//             ${fullName}
-//             </p>
-//        </div>
-//        `
-//         container.appendChild(parkCard);
-//     });
-// });
 
-// })
-
-// setTimeout(() => {
-//     const openModal = document.querySelectorAll('.park');
-
-//     console.log(openModal);
-// }, 2000)
-
-
-
-
-// for (const elm of openModal) {
-//     console.log(elm);
-//     // elm.addEventListener('click', function(){
-//     //     const parkCode = this.dataset.open;
-//     //     this.classList.add(isVisible)
-//     // })
-// }
 
 
 
